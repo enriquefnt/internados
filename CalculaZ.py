7c44d450f2201506be533397fe9594dc817b1b6e
enriquefnt <enriquefinetti@gmail.com>
2025-02-18 14:35:29 -0300

    calculos

--- CalculaZ.py
+++ CalculaZ.py
@@ -8,20 +8,6 @@ db_config = {
     "password": "enfi7625",
     "database": "saltaped_sivin2" 
 }
-
-# Leer el archivo de Excel
-archivo_excel = "./planillas/consolidado.xlsx"  
-df = pd.read_excel(archivo_excel, dtype=str)  # Leer todo como string para validaciones
-
-# Conectar a MySQL
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
-
-# Crear nuevas columnas para los Z-scores
-df["Z_PesoEdad"] = "error"
-df["Z_TallaEdad"] = "error"
-df["Z_IMCEdad"] = "error"
-
 # Función para convertir fechas al formato correcto
 def convertir_fecha(fecha):
     try:
@@ -36,6 +22,32 @@ def convertir_numero(valor):
         return num / 1000 if num > 1000 else num  # Convierte gramos a kg si es necesario
     except ValueError:
         return None  # Si no se puede convertir, devuelve None
+# Leer el archivo de Excel
+archivo_excel = "./planillas/consolidado.xlsx"  
+df = pd.read_excel(archivo_excel, dtype=str)  # Leer todo como string para validaciones
+df = df.drop_duplicates(subset=["DNI", "FECHA INGRESO"], keep="last")
+# Ordenar la tabla por fecha de ingreso
+df = df.sort_values(by=["DNI", "FECHA INGRESO"])
+# Contar ocurrencias acumuladas de cada DNI
+df["REINGRESO"] = df.groupby("DNI").cumcount() + 1  # Suma 1 para empezar desde 1
+df["FECHA INGRESO"] = pd.to_datetime(df["FECHA INGRESO"], errors="coerce")
+df["FECHA EGRESO"] = pd.to_datetime(df["FECHA EGRESO"], errors="coerce")
+
+df["DIAS ESTADA"] = (df["FECHA EGRESO"] - df["FECHA INGRESO"]).dt.days
+
+# Conectar a MySQL
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+# Crear nuevas columnas para los Z-scores
+df["Z_PesoEdad_ing"] = "error"
+df["Z_TallaEdad_ing"] = "error"
+df["Z_IMCEdad_ing"] = "error"
+df["Z_PesoEdad_egr"] = "error"
+df["Z_TallaEdad_egr"] = "error"
+df["Z_IMCEdad_egr"] = "error"
+
+
 
 
 # Iterar sobre cada fila del DataFrame
@@ -45,23 +57,43 @@ for index, row in df.iterrows():
     fecha_control = convertir_fecha(row["FECHA INGRESO"])
     peso = convertir_numero(row["PESO INGRESO (kg)"])
     talla = convertir_numero(row["TALLA INGRESO (cm)"])
-    print(peso,talla,fecha_control,fecha_nace,sexo)
+    fecha_control_e = convertir_fecha(row["FECHA EGRESO"])
+    peso_e = convertir_numero(row["PESO EGRESO (kg)"])
+    talla_e = convertir_numero(row["TALLA EGRESO (cm)"])
+    
+    # print(peso,talla,fecha_control,fecha_nace,sexo)
     try:
-        # Calcular Z-Score de Peso/Edad
+        # Calcular Z-Score de Peso/Edad ingreso
         if peso is not None and fecha_nace and fecha_control:
             cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 'p', peso, fecha_nace, fecha_control))
-            df.at[index, "Z_PesoEdad"] = cursor.fetchone()[0] or "error"
+            df.at[index, "Z_PesoEdad_ing"] = cursor.fetchone()[0] or "error"
 
-        # Calcular Z-Score de Talla/Edad
+        # Calcular Z-Score de Talla/Edad ingreso
         if talla is not None and fecha_nace and fecha_control:
             cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 't', talla, fecha_nace, fecha_control))
-            df.at[index, "Z_TallaEdad"] = cursor.fetchone()[0] or "error"
-
-        # Calcular Z-Score de IMC/Edad
+            df.at[index, "Z_TallaEdad_ing"] = cursor.fetchone()[0] or "error"
+ 
+        # Calcular Z-Score de IMC/Edad ingreso
         if peso is not None and talla is not None and fecha_nace and fecha_control:
             imc = peso / ((talla / 100) ** 2)
             cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 'i', imc, fecha_nace, fecha_control))
-            df.at[index, "Z_IMCEdad"] = cursor.fetchone()[0] or "error"
+            df.at[index, "Z_IMCEdad_ing"] = cursor.fetchone()[0] or "error"
+        
+        # Calcular Z-Score de Peso/Edad egreso
+        if peso is not None and fecha_nace and fecha_control_e:
+            cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 'p', peso_e, fecha_nace, fecha_control_e))
+            df.at[index, "Z_PesoEdad_egr"] = cursor.fetchone()[0] or "error"
+
+        # Calcular Z-Score de Talla/Edad egreso
+        if talla is not None and fecha_nace and fecha_control_e:
+            cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 't', talla_e, fecha_nace, fecha_control_e))
+            df.at[index, "Z_TallaEdad_egr"] = cursor.fetchone()[0] or "error"
+ 
+        # Calcular Z-Score de IMC/Edad egreso
+        if peso is not None and talla is not None and fecha_nace and fecha_control_e:
+            imc = peso_e / ((talla_e / 100) ** 2)
+            cursor.execute("SELECT ZSCORE(%s, %s, %s, %s, %s)", (sexo, 'i', imc, fecha_nace, fecha_control_e))
+            df.at[index, "Z_IMCEdad_egr"] = cursor.fetchone()[0] or "error"    
 
     except mysql.connector.Error as err:
         print(f"Error en la fila {index}: {err}")
